9823f91a8b4da53e557c5172f744ac425a9dd023
Aarish <aarishm767@gmail.com>
2023-03-29 16:39:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/demodeployedapplication/DemoDeployedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodeployedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodeployedapplication/DemoDeployedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeployedapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
